04ad38ec4b8f9de9a72f4183896ef01189953a03
Solovyov <max-solovyov@rambler.ru>
2021-01-22 18:37:47 +0300

    v 0.2

--- JDI/JDI/Support/SceneDelegate.swift
+++ JDI/JDI/Support/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = TasksViewController()
+        window?.rootViewController = UINavigationController(rootViewController: TasksViewController())
         window?.makeKeyAndVisible()
     }
 
